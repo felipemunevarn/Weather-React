d185c2430c53516e83f3be25ef310d8be95179a7
unknown <afmunene@gmail.com>
2020-11-10 14:12:01 -0300

    second commit

---
+++ build/precache-manifest.0b6bbaf451948e5d3a33b6deeccd9c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4ba00900b71c62bfec895a59b4b021",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "dbd4fa6922572bf70053",
+    "url": "/Weather-React/static/css/main.551659fb.chunk.css"
+  },
+  {
+    "revision": "916994e5f8503330f61b",
+    "url": "/Weather-React/static/js/2.4f3af66b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Weather-React/static/js/2.4f3af66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd4fa6922572bf70053",
+    "url": "/Weather-React/static/js/main.10b7e816.chunk.js"
+  },
+  {
+    "revision": "cc424d37371af829423a",
+    "url": "/Weather-React/static/js/runtime-main.7f1b0a88.js"
+  }
+]);
